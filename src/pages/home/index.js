0bdefb4c40a0b4d4cb1863acf383575110ed0f56
Alam <alam_duran@hotmail.com>
2022-02-11 21:17:51 -0600

    List select

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,15 +2,58 @@ import React from 'react';
 
 const Home = () => {
     return ( 
-        <div class="container">
-        <div class="container__content">
-            <div classe="container__img">
-                <img src="/public/assets/img/airport.png" alt="Airport"/>
+        <div className="container">
+        <div className="container__content">
+            <div className="container__img">
+                <img src="/assets/img/airport.png" alt="Airport"/>
             </div>
 
-            <div class="container__form">
-                <form action="" class="container__input">
-                    <h1 class="container__title">Busca ofertas de vuelos</h1>
+            <div className="container__forms">
+                <form action="" className="container__form">
+                    <h1 className="container__title">Busca ofertas de vuelos</h1>
+
+                    <label>
+                        Origen:
+                        <select className="container__input">
+                            <option value="1">Uno</option>
+                            <option value="2">Dos</option>
+                            <option selected value="3">Guadalajara</option>
+                            <option value="4">Cuatro</option>
+                        </select>
+                    </label>
+
+                    <label>
+                        Destino:
+                        <select className="container__input">
+                            <option value="1">Uno</option>
+                            <option value="2">Dos</option>
+                            <option selected value="3">México</option>
+                            <option value="4">Cuatro</option>
+                        </select>
+                    </label>
+
+                    <label>
+                        Fecha:
+                        <select className="container__input">
+                            <option value="1">Uno</option>
+                            <option value="2">Dos</option>
+                            <option selected value="3">Lunes, 15 de febrero de 2021 8:00</option>
+                            <option value="4">Cuatro</option>
+                        </select>
+                    </label>
+
+                    <label>
+                        Pasajeros:
+                        <select className="container__input">
+                            <option value="1">Uno</option>
+                            <option value="2">Dos</option>
+                            <option selected value="3">2 Adultos</option>
+                            <option value="4">Cuatro</option>
+                        </select>
+                    </label>
+                 
+                    <button href="#" className="">Buscar vuelos</button>
+                    
 
                 </form>
             </div>
