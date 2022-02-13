0ae7dc4b94be868f755ec81aee04f376214e52d8
Alammduran <alam_duran@hotmail.com>
2022-02-12 22:02:10 -0600

    flights route

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Reservations from "./pages/reservations";
+import Flights from "./pages/flights";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route exact path="/vuelos" element={<Flights />} />
       <Route exact path="/reservaciones" element={<Reservations />} />
     </Routes>
   );
