d330992026482dab0eab94de0d74d49f41218453
Alam <alam_duran@hotmail.com>
2022-02-13 21:45:43 -0600

    card css

--- src/components/flight-card/index.js
+++ src/components/flight-card/index.js
@@ -1,23 +1,43 @@
 import React from "react";
 import moment from "moment";
+import "./style.css";
 
 const FlightCard = ({ flight, handleSelected }) => {
   const { id, origin, destination, departure_date, price } = flight;
   return (
-    <div className="container__box">
-      <p>{id}</p>
-      <p>{origin}</p>
-      <p>{destination}</p>
-      <p>{moment(departure_date).format("LLLL")}</p>
-      <p>
-        {price.toLocaleString("es-MX", {
-          style: "currency",
-          currency: "MXN",
-          minimumFractionDigits: "2",
-        })}
-      </p>
-      <button onClick={() => handleSelected(id)}>Pre-reservar</button>
+    <div className="container">
+
+      <div className="card__grid">
+        <article>
+          <img src="/assets/img/card.svg" alt="Card" />
+
+          <div className="card__content">
+
+            <div>
+              <p><strong>Origen:</strong> {origin}</p>
+              <p><strong>Destino:</strong> {destination}</p>
+            </div>
+
+            <div>
+              <p> <strong>Precio</strong>
+                {price.toLocaleString("es-MX", {
+                  style: "currency",
+                  currency: "MXN",
+                  minimumFractionDigits: "2",
+                })}
+              </p>
+              <p><strong>Fecha:</strong> {moment(departure_date).format("LLLL")}</p>
+            </div>
+
+            <button onClick={() => handleSelected(id)}>Siguiente</button>
+
+          </div>
+        </article>
+      </div>
+
+
     </div>
+
   );
 };
 
